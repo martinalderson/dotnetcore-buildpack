a118ec6f06335fab4905977839718941d3975d7e
jincod <vadim.abdrashitov@gmail.com>
2026-04-23 11:50:41 +0500

    updated, try to fix the test

--- test/fixtures/local-tools/Program.cs
+++ test/fixtures/local-tools/Program.cs
@@ -1,25 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
 
-namespace local_tools
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
+app.MapGet("/", () => "Hello World!");
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
-    }
-}
+app.Run();
